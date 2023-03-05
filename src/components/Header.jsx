fb485265787b0cb65503ba581fdb5783903ae61b
Krystian Florkowski <86536950+Krytianf06@users.noreply.github.com>
2023-03-05 01:23:58 +0100

    MoveClick

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./header.css";
-// import { useEffect, useState } from "react";
+import { useState } from "react";
 
 function Header() {
-	// const [lok, setLok] = useState();
+	const [list, setList] = useState("none");
 
 	// useEffect(() => {
 	// 	const tablica = localStorage.getItem("tablica");
@@ -12,11 +12,19 @@ function Header() {
 	// 	setLok(numer);
 	// }, [lok]);
 
-	// const henderKcik = () => {
-	// 	const sum = lok + 1;
-	// 	console.log(sum);
-	// 	localStorage.setItem("tablica", JSON.stringify(sum));
-	// 	setLok(sum);
+	const clickListMove = (ev) => {
+		console.log(ev.type);
+		if (ev.type === "mouseenter") {
+			setList("block");
+		} else if (ev.type === "mouseleave") {
+			setList("none");
+		}
+	};
+	// const clickList = (ev) => {
+	// 	console.log(ev.type);
+	// 	if (list === "none") {
+	// 		setList("block");
+	// 	} else setList("none");
 	// };
 
 	return (
@@ -69,8 +77,10 @@ function Header() {
 								</svg>
 							</a>
 						</li>
-						<li className='heder_nav_list_item drop'>
-							<a className='btn_ikon' href='przyciski'>
+						<li
+							onMouseEnter={clickListMove}
+							className='heder_nav_list_item drop'>
+							<p className='btn_ikon' href='przyciski'>
 								Produkty
 								<svg
 									version='1.1'
@@ -81,11 +91,20 @@ function Header() {
 									<path d='M32 29c0 1.657-1.343 3-3 3s-3-1.343-3-3c0-1.657 1.343-3 3-3s3 1.343 3 3z'></path>
 									<path d='M32 16v-12h-24c0-1.105-0.895-2-2-2h-6v2h4l1.502 12.877c-0.915 0.733-1.502 1.859-1.502 3.123 0 2.209 1.791 4 4 4h24v-2h-24c-1.105 0-2-0.895-2-2 0-0.007 0-0.014 0-0.020l26-3.98z'></path>
 								</svg>
-							</a>
-							<ul className='drop_list'>
-								<li>Pelet</li>
-								<li>Instalacje C.O.</li>
-								<li>Brukarstwo</li>
+							</p>
+							<ul
+								onMouseLeave={clickListMove}
+								className='drop_list'
+								style={{ display: list }}>
+								<li>
+									<a href='przyciski'>Pelet</a>
+								</li>
+								<li>
+									<a href='przyciski'>Instalacje C.O.</a>
+								</li>
+								<li>
+									<a href='przyciski'>Brukarstwo</a>
+								</li>
 							</ul>
 						</li>
 						<li className='heder_nav_list_item'>
