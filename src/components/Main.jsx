49b3e8949d71423ee8a4ac147c3953286b657cdb
Krystian Florkowski <krystianf06@gmail.com>
2023-03-17 23:00:41 +0100

    box

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
-import './main.css';
-
+import React from "react";
+import "./main.css";
 
 function Main() {
-  return (
-    <div className="conteiner">
-     <p>Main</p>
-    </div>
-  );
+	return (
+		<div className='conteiner'>
+			<div className='box'>
+				<div className='box-item'>
+					<h1>Pellet</h1>
+				</div>
+				<div className='box-item'>
+					<h1>Kostka Brukowa</h1>
+				</div>
+				<div className='box-item'>
+					<h1>Hydraaulika Wod-Kan i C.O.</h1>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Main;
