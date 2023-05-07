d92ef122b71048446e7968d54bf0a11bad9b553b
Krystian Florkowski <krystianf06@gmail.com>
2023-05-07 13:20:31 +0200

    router

--- src/App.js
+++ src/App.js
@@ -4,9 +4,8 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Base from "./components/base/Base";
 import Home from "./components/Home/Home";
 import Products1 from "./components/products/Products1";
-import Products2 from './components/products/Products2';
-import Products3 from './components/products/Products3';
-
+import Products2 from "./components/products/Products2";
+import Products3 from "./components/products/Products3";
 
 const router = createBrowserRouter([
 	{
@@ -20,17 +19,14 @@ const router = createBrowserRouter([
 			{
 				path: "klassaa1",
 				element: <Products1 />,
-				
 			},
 			{
 				path: "agropellet",
 				element: <Products2 />,
-				
 			},
 			{
 				path: "przemyslowy",
 				element: <Products3 />,
-				
 			},
 		],
 	},
