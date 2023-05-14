96b4333b35a5c19f52e3b200792fd320e14f3de5
Krystian Florkowski <86536950+Krytianf06@users.noreply.github.com>
2023-05-14 18:33:57 +0200

    poprawki

--- src/App.js
+++ src/App.js
@@ -1,42 +1,29 @@
 import React from "react";
 import "./App.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+	createBrowserRouter,
+	RouterProvider,
+	createRoutesFromElements,
+	Route,
+} from "react-router-dom";
+// import Router from 'react-easy-router';
 import Base from "./components/base/Base";
 import Home from "./components/Home/Home";
 import Products1 from "./components/products/Products1";
 import Products2 from "./components/products/Products2";
 import Products3 from "./components/products/Products3";
 
-
-
 function App() {
-	const router = createBrowserRouter([
-		{
-			path: "/",
-			element: <Base />,
-			children: [
-				{
-					index: true,
-					element: <Home />,
-				},
-				{
-					path: "klassaa1",
-					element: <Products1 />,
-				},
-				{
-					path: "agropellet",
-					element: <Products2 />,
-				},
-				{
-					path: "przemyslowy",
-					element: <Products3 />,
-				},
-			],
-		},
-	]);
-
-
-
+	const router = createBrowserRouter(
+		createRoutesFromElements(
+			<Route path='/' element={<Base />}>
+				<Route path='home' element={<Home />} />
+				<Route path='klassaa1' element={<Products1 />} />
+				<Route path='agropellet' element={<Products2 />} />
+				<Route path='przemyslowy' element={<Products3 />} />
+			</Route>
+		)
+	);
 	return <RouterProvider router={router} />;
 }
 
