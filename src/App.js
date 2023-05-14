69c28afd84da52b39aa9041fd937712bbc3820e2
Krystian Florkowski <86536950+Krytianf06@users.noreply.github.com>
2023-05-14 18:14:38 +0200

    poprawki

--- src/App.js
+++ src/App.js
@@ -7,32 +7,36 @@ import Products1 from "./components/products/Products1";
 import Products2 from "./components/products/Products2";
 import Products3 from "./components/products/Products3";
 
-const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <Base />,
-		children: [
-			{
-				index: true,
-				element: <Home />,
-			},
-			{
-				path: "klassaa1",
-				element: <Products1 />,
-			},
-			{
-				path: "agropellet",
-				element: <Products2 />,
-			},
-			{
-				path: "przemyslowy",
-				element: <Products3 />,
-			},
-		],
-	},
-]);
+
 
 function App() {
+	const router = createBrowserRouter([
+		{
+			path: "/",
+			element: <Base />,
+			children: [
+				{
+					index: true,
+					element: <Home />,
+				},
+				{
+					path: "klassaa1",
+					element: <Products1 />,
+				},
+				{
+					path: "agropellet",
+					element: <Products2 />,
+				},
+				{
+					path: "przemyslowy",
+					element: <Products3 />,
+				},
+			],
+		},
+	]);
+
+
+
 	return <RouterProvider router={router} />;
 }
 
