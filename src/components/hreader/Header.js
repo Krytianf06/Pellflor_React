a4176669867709c10fa858a565a029711b9aa4e3
Krystian Florkowski <krystianf06@outlook.com>
2025-01-26 17:33:27 +0100

    poprawki usunięcie

--- src/components/hreader/Header.js
+++ src/components/hreader/Header.js
@@ -6,30 +6,13 @@ import imageSlider from "../data";
 
 function Header() {
 
-	function fetchUsers() {
-		fetch('https://amunatcoll.pl:8000/login/',
-		  {
-			mode: 'cors',
-			method: 'POST',
-			headers: {
-			  'Accept': 'application/json',
-			  'Content-Type': 'application/json',
-			  'access-control-allow-credentials': 'true',
-			}, body: JSON.stringify({
-			  "username": "kf63083@amu.edu.pl",
-			  "password": "Krychu11L19*63083"
-			})
-		  })
-		  .then(res => res.json())
-		  .then((dane) => console.log(dane))
-	  };
+	
 	const [currentState, setCurrentState] = useState(1);
 	useEffect(() => {
 		const timer = setTimeout(() => {
 			if (currentState === 2) {
 				setCurrentState(0);
 				console.log("klops zmiana");
-				fetchUsers();
 			} else {
 				setCurrentState(currentState + 1);
 			}
